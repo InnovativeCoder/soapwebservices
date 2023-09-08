8059fca105e018a11528bf4e0d21d7597990ce48
InnovativeCoder <aashitsingh2010@gmail.com>
2023-09-08 18:44:14 -0400

    request response working fine now

--- src/main/java/dev/jerry/soapwebservices/soap/CourseDetailsEndpoint.java
+++ src/main/java/dev/jerry/soapwebservices/soap/CourseDetailsEndpoint.java
@@ -7,14 +7,14 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-//import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
 
 
-@Endpoint("")
+@Endpoint
 public class CourseDetailsEndpoint {
-    @PayloadRoot(namespace ="http://javacourses.com/courses", localPart = "GetCourseDetailsRequest")
+    @PayloadRoot(namespace ="http://javacourses.com/courses", localPart = "GetCourseDetailRequest")
     @ResponsePayload
-    public GetCourseDetailResponse processCourseDetailsRequest(
+    public GetCourseDetailResponse processCourseDetailRequest(
             @RequestPayload
             GetCourseDetailRequest request){
         GetCourseDetailResponse response = new GetCourseDetailResponse();
@@ -22,6 +22,7 @@ public class CourseDetailsEndpoint {
         courseDetails.setId(request.getId());
         courseDetails.setName("Microservice Course");
         courseDetails.setDescription("This is a course on Microservices");
+        response.setCourseDetails(courseDetails);
         return response;
     }
 }
