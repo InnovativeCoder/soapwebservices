b989829477d35a67760f7d11a6b6c27ef18f9c2c
InnovativeCoder <aashitsingh2010@gmail.com>
2023-09-09 12:30:42 -0400

    added more api calls, endpoint

--- src/main/java/dev/jerry/soapwebservices/soap/CourseDetailsEndpoint.java
+++ src/main/java/dev/jerry/soapwebservices/soap/CourseDetailsEndpoint.java
@@ -3,6 +3,10 @@ package dev.jerry.soapwebservices.soap;
 import com.javacourses.courses.CourseDetails;
 import com.javacourses.courses.GetCourseDetailRequest;
 import com.javacourses.courses.GetCourseDetailResponse;
+import dev.jerry.soapwebservices.soap.bean.Course;
+import dev.jerry.soapwebservices.soap.service.CourseDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -12,16 +16,22 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 
 @Endpoint
 public class CourseDetailsEndpoint {
+
+    @Autowired
+    CourseDetailsService service;
+
     @PayloadRoot(namespace ="http://javacourses.com/courses", localPart = "GetCourseDetailRequest")
     @ResponsePayload
     public GetCourseDetailResponse processCourseDetailRequest(
             @RequestPayload
             GetCourseDetailRequest request){
+        Course course = service.findById(request.getId());
+
         GetCourseDetailResponse response = new GetCourseDetailResponse();
         CourseDetails courseDetails = new CourseDetails();
-        courseDetails.setId(request.getId());
-        courseDetails.setName("Microservice Course");
-        courseDetails.setDescription("This is a course on Microservices");
+        courseDetails.setId(course.getId());
+        courseDetails.setName(course.getName());
+        courseDetails.setDescription(course.getDescription());
         response.setCourseDetails(courseDetails);
         return response;
     }
