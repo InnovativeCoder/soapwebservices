4bf5f3fd287426d93efa1c029217351f9d9172f0
InnovativeCoder <aashitsingh2010@gmail.com>
2023-09-11 01:44:06 -0400

    Added ws seecurity

--- src/main/java/dev/jerry/soapwebservices/soap/WebServiceConfig.java
+++ src/main/java/dev/jerry/soapwebservices/soap/WebServiceConfig.java
@@ -7,16 +7,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+import java.util.List;
+
 //Enable Spring Web services
 @EnableWs
 //Spring Configuration
 @Configuration
-public class WebServiceConfig {
+public class WebServiceConfig extends WsConfigurerAdapter {
     //MessageDispatcherServlet
         //ApplicationContext
     // url-> /ws/*
@@ -46,5 +51,22 @@ public class WebServiceConfig {
     @Bean
     public XsdSchema courseSchema(){
         return new SimpleXsdSchema(new ClassPathResource("course-details.xsd"));
+    }
+
+
+    @Bean
+    public Wss4jSecurityInterceptor securityInterceptor() {
+        Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
+        securityInterceptor.setSecurementActions("UsernameToken");
+        securityInterceptor.setSecurementUsername("user");
+        securityInterceptor.setSecurementPassword("password");
+
+        return securityInterceptor;
+    }
+
+    // Interceptors.add -> XwsSecurityInterceptor
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(securityInterceptor());
     }
 }
